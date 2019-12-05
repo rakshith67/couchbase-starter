3b080d0749dc6666b61eb63ca1eeb6e71a3db5eb
Rakshith <rakshithmamidala67@gmail.com>
2019-12-05 18:03:25 +0530

    First Commit

---
+++ src/main/java/couchbase/couchbase_starter/App.java
@@ -0,0 +1,13 @@
+package couchbase.couchbase_starter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
